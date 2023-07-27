c43a5b4f69ecf55cd5e012c868df9430a80012c8
katesyvash795 <126354194+katesyvash795@users.noreply.github.com>
2023-07-27 10:33:53 +0300

    :

--- src/cat-api.js
+++ src/cat-api.js
@@ -4,17 +4,16 @@ axios.defaults.headers.common['x-api-key'] =
   'live_H24JXYBVrrM9Zqd0AhOom07voavPNPg9MNCpoSpk17G91i6mXMnj02i3p6IfEKdP';
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
 
-const breedsEndpoint = 'breeds';
-const catsEndpoint = 'images/search';
+const endPoint = 'breeds'; // Змінна зі сталою величиною для URL
 
 export function fetchBreeds() {
-  return axios.get(breedsEndpoint).then(data => {
+  return axios.get(endPoint).then(data => {
     return data.data;
   });
 }
 
 export function fetchCatByBreed(breedId) {
-  const catByBreed = `${catsEndpoint}?breed_ids=${breedId}`;
+  const catByBreed = `${endPoint}/${breedId}`; // Використання змінної endPoint
   return axios.get(catByBreed).then(resp => {
     return resp.data;
   });

c4fd092eddc9aad660c07edc2a36ecff9af3307e
katesyvash795 <126354194+katesyvash795@users.noreply.github.com>
2023-07-27 10:51:10 +0300

    L

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
-  'live_H24JXYBVrrM9Zqd0AhOom07voavPNPg9MNCpoSpk17G91i6mXMnj02i3p6IfEKdP';
-axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
+  'live_0AUTe1rdtPbkwAintnktfaswtDRd2q3eYxXu5ywHbfzBuQ6yGBUeqoZzeCK7pBJX';
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/breeds';
 
 
 export function fetchBreeds(endPoint) {
